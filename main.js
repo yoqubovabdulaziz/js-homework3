8a1d95c83d5d3ff705445c19bec5b02b27300f83
yoqubovabdulaziz <abdulazizdasturlash@gmail.com>
2023-12-13 01:18:39 +0500

    finished

--- main.js
+++ main.js
@@ -547,4 +547,308 @@
 
 // case 8
 
+// let Y = prompt("Yo'nalishni kiriting sonda kiriting!");
+// let K = prompt("Xarakatni kiriting!");
 
+// let z = Y + K;
+
+// let res;
+
+// switch (z) {
+//   case "s0":
+//     res = "Robot shimolga qarab harakatni dovom ettirdi";
+//     break;
+//   case "s1":
+//     res = "Robot g'arbga qarab turipti";
+//     break;
+//   case "s2":
+//     res = "Robot sharqqa qarab turipti";
+//     break;
+//   case "j0":
+//     res = "Robot janubga qarab harakatni dovom ettirdi";
+//     break;
+//   case "j1":
+//     res = "Robot sharqqa qarab turipti";
+//     break;
+//   case "j2":
+//     res = "Robot g'arbga qarab turipti";
+//     break;
+//   case "q0":
+//     res = "Robot sharqqa qarab harakatni dovom ettirdi";
+//     break;
+//   case "q1":
+//     res = "Robot shimolga qarab turipti";
+//     break;
+//   case "q2":
+//     res = "Robot janubga qarab turipti";
+//     break;
+//   case "g0":
+//     res = "Robot g'arbga qarab harakatni dovom ettirdi";
+//     break;
+//   case "g1":
+//     res = "Robot janubga qarab turipti";
+//     break;
+//   case "g2":
+//     res = "Robot shimolga qarab turipti";
+//     break;
+//   default:
+//     res = "Xato";
+//     break;
+// }
+
+// console.log(res);
+
+// case 9
+
+// let M = 4;
+// let N = 10;
+
+// if (N == 6) {
+//   N = "olti"
+// } else if (N == 7) {
+//   N = "yetti"
+// } else if (N == 8) {
+//   N = "sakkiz"
+// } else if (N == 9) {
+//   N = "to'qqiz"
+// } else if (N == 10) {
+//   N = "o'n"
+// } else if (N == 11) {
+//   N = "valet"
+// } else if (N == 12) {
+//   N = "dama"
+// } else if (N == 13) {
+//   N = "qirol"
+// } else if (N == 14) {
+//   N = "tuz"
+// } else {
+//   M = 0
+// };
+
+// let res;
+
+// switch (M) {
+//   case 1:
+//     res = N + " g'isht";
+//     break;
+//   case 2:
+//     res = N + " olma";
+//     break;
+//   case 3:
+//     res = N + " chillak";
+//     break;
+//   case 4:
+//     res = N + " qarg'a";
+//     break;
+//   default:
+//     res = "Xato";
+//     break;
+// }
+
+// console.log(res);
+
+// case 10
+
+// let M = 20;
+
+// let bir = M % 10;
+// let on = (M - bir) / 10;
+// let N
+
+// if (bir == 0) {
+//   N = ""
+// } else if (bir == 1) {
+//   N = " bit"
+// } else if (bir == 2) {
+//   N = " ikki"
+// } else if (bir == 3) {
+//   N = " uch"
+// } else if (bir == 4) {
+//   N = " to'rt"
+// } else if (bir == 5) {
+//   N = " besh"
+// } else if (bir == 6) {
+//   N = " olti"
+// } else if (bir == 7) {
+//   N = " yetti"
+// } else if (bir == 8) {
+//   N = " sakkiz"
+// } else if (bir == 9) {
+//   N = " to'qqiz"
+// };
+
+// let res;
+
+// switch (on) {
+//   case 1:
+//     res = "O'n" + N + "ta masala";
+//     break;
+//   case 2:
+//     res = "Yigirma" + N + "ta masala";
+//     break;
+//   case 3:
+//     res = "O'ttiz" + N + "ta masala";
+//     break;
+//   case 4:
+//     res = "Qirq" + N + "ta masala";
+//     break;
+//   default:
+//     res = "Xato";
+//     break;
+// }
+
+// console.log(res);
+
+// case 11
+
+// let A = 699;
+
+// let bir = A % 10;
+// let on = (A % 100 - bir) / 10;
+// let yuz = (A - on * 10 - bir) / 100;
+// let N;
+// let M;
+
+// if (bir == 0) {
+//   N = ""
+// } else if (bir == 1) {
+//   N = " bir"
+// } else if (bir == 2) {
+//   N = " ikki"
+// } else if (bir == 3) {
+//   N = " uch"
+// } else if (bir == 4) {
+//   N = " to'rt"
+// } else if (bir == 5) {
+//   N = " besh"
+// } else if (bir == 6) {
+//   N = " olti"
+// } else if (bir == 7) {
+//   N = " yetti"
+// } else if (bir == 8) {
+//   N = " sakkiz"
+// } else if (bir == 9) {
+//   N = " to'qqiz"
+// };
+
+// if (on == 0) {
+//   M = "";
+// } else if (on == 1) {
+//   M = " o'n";
+// } else if (on == 2) {
+//   M = " yigirma";
+// } else if (on == 3) {
+//   M = " o'ttiz";
+// } else if (on == 4) {
+//   M = " qirq";
+// } else if (on == 5) {
+//   M = " ellik";
+// } else if (on == 6) {
+//   M = " oltimish";
+// } else if (on == 7) {
+//   M = " yetmish";
+// } else if (on == 8) {
+//   M = " sakson";
+// } else if (on == 9) {
+//   M = " to'qson";
+// }
+
+// let res;
+
+// switch (yuz) {
+//   case 1:
+//     res = "bir yuz" + M + N;
+//     break;
+//     case 2:
+//       res = "ikki yuz" + M + N;
+//       break;
+//   case 3:
+//     res = "uch yuz" + M + N;
+//     break;
+//   case 4:
+//     res = "to'rt yuz" + M + N;
+//     break;
+//   case 5:
+//     res = "besh yuz" + M + N;
+//     break;
+//   case 6:
+//     res = "olti yuz" + M + N;
+//     break;
+//   case 7:
+//     res = "yetti yuz" + M + N;
+//     break;
+//   case 8:
+//     res = "sakkiz yuz" + M + N;
+//     break;
+//   case 9:
+//     res = "to'qqiz yuz" + M + N;
+//     break;
+//   default:
+//     res = "Xato";
+//     break;
+// };
+
+// console.log(res);
+
+// case 12
+
+// let A = 562358762358;
+
+// let asr = (A + 57) % 60;
+// let rang = Math.ceil(asr / 12);
+// let hayvon = asr % 12;
+// console.log(rang);
+
+// let M;
+
+// if (hayvon == 0) {
+//   M = " to'ng'iz";
+// } else if (hayvon == 1) {
+//   M = " sichqon";
+// } else if (hayvon == 2) {
+//   M = " sigir";
+// } else if (hayvon == 3) {
+//   M = " yo'lbars";
+// } else if (hayvon == 4) {
+//   M = " quyon";
+// } else if (hayvon == 5) {
+//   M = " ajdar";
+// } else if (hayvon == 6) {
+//   M = " ilon";
+// } else if (hayvon == 7) {
+//   M = " ot";
+// } else if (hayvon == 8) {
+//   M = " qo'y";
+// } else if (hayvon == 9) {
+//   M = " maymun";
+// } else if (hayvon == 9) {
+//   M = " tovuq";
+// } else if (hayvon == 9) {
+//   M = " it";
+// }
+
+// let res;
+
+// switch (rang) {
+//   case 0:
+//   case 5:
+//     res = "Qora" + M;
+//     break;
+//   case 1:
+//     res = "Yashil" + M;
+//     break;
+//   case 2:
+//     res = "Qizil" + M;
+//     break;
+//   case 3:
+//     res = "Sariq" + M;
+//     break;
+//   case 4:
+//     res = "Oq" + M;
+//     break;
+//   default:
+//     res = "Xato";
+//     break;
+// }
+
+// console.log(res);
